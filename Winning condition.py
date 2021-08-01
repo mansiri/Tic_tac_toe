cfab9fa609c73aac5ce49546f73054a032f42d7f
Man.Siri <sirixdsg@gmail.com>
2021-08-01 11:51:32 -0400

    Create Winning condition.py

---
+++ Winning condition.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+       if input >= 5:
+           if A[1] == A[2] == A[3]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+           elif B[1] == B[2] == B[3]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+           elif C[1] == C[2] == [3]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+           elif A[1] == B[2] == C[3]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+           elif C[1] == B[2] == A[3]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+           elif A[1] == B[1] == C[1]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+           elif A[2] == B[2] == C[2]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+           elif A[3] == B[3] == C[3]:
+               print(gameboard)
+               print("\nGame Over.\n")                
+               break
+
+    
+       if input == 9:
+           print("\nGame Over.\n")                
+           print("It's a Tie!!")
+
+      
+
